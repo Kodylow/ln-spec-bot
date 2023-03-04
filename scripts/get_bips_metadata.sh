c50881792dbb6406cc3658a1a21edb0222d3ae45
kody.low <kodylow7@gmail.com>
2023-03-03 21:27:26 -0800

    hold

--- scripts/get_bips_metadata.sh
+++ scripts/get_bips_metadata.sh
@@ -3,22 +3,19 @@
 mkdir -p ../metadata/
 
 for file in ../bips/*.md; do
-    echo "Processing ${file}..."
-    BIP="$(grep -i '^bip:' "${file}" | awk '{print $2}')"
-    LAYER="$(grep -i '^layer:' "${file}" | awk '{$1="";print$0}' | sed 's/^ *//g')"
-    TITLE="$(grep -i '^title:' "${file}" | awk '{$1="";print$0}' | sed 's/^ *//g')"
-    AUTHOR="$(grep -i '^author:' "${file}" | awk '{$1="";print$0}' | sed 's/^ *//g')"
-    COMMENTS_SUMMARY="$(grep -i '^comments-summary:' "${file}" | awk '{$1="";print$0}' | sed 's/^ *//g')"
-    COMMENTS_URI="$(grep -i '^comments-uri:' "${file}" | awk '{$1="";print$0}' | sed 's/^ *//g')"
-    STATUS="$(grep -i '^status:' "${file}" | awk '{$1="";print$0}' | sed 's/^ *//g')"
-    TYPE="$(grep -i '^type:' "${file}" | awk '{$1="";print$0}' | sed 's/^ *//g')"
-    CREATED="$(grep -i '^created:' "${file}" | awk '{$1="";print$0}' | sed 's/^ *//g')"
+  echo "Processing ${file}..."
+  BIP="$(grep -i '^bip:' "${file}" | awk '{print $2}')"
+  BIP_PREFIX="$(printf "bip-%04d" "${BIP#0}")"
+  LAYER="$(grep -i '^layer:' "${file}" | awk '{$1="";print$0}' | sed 's/^ *//g')"
+  TITLE="$(grep -i '^title:' "${file}" | awk '{$1="";print$0}' | sed 's/^ *//g')"
+  AUTHOR="$(grep -i '^author:' "${file}" | awk '{$1="";print$0}' | sed 's/^ *//g')"
+  COMMENTS_SUMMARY="$(grep -i '^comments-summary:' "${file}" | awk '{$1="";print$0}' | sed 's/^ *//g')"
+  COMMENTS_URI="$(grep -i '^comments-uri:' "${file}" | awk '{$1="";print$0}' | sed 's/^ *//g')"
+  STATUS="$(grep -i '^status:' "${file}" | awk '{$1="";print$0}' | sed 's/^ *//g')"
+  TYPE="$(grep -i '^type:' "${file}" | awk '{$1="";print$0}' | sed 's/^ *//g')"
+  CREATED="$(grep -i '^created:' "${file}" | awk '{$1="";print$0}' | sed 's/^ *//g')"
 
-    # Get the filename without the path and extension
-    filename=$(basename -- "${file}")
-    filename="${filename%.*}"
-
-    cat << EOF > "../metadata/${filename}.json"
+  cat <<EOF >"../metadata/${BIP_PREFIX}.json"
 {
   "BIP": "${BIP}",
   "Layer": "${LAYER}",
